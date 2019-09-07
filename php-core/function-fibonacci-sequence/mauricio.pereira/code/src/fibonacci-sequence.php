f74430462282ca6f25fc45ec05f008b820f4b246
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2019-09-06 23:03:28 -0300

    solution fibonacci-sequence

---
+++ php-core/function-fibonacci-sequence/mauricio.pereira/code/src/fibonacci-sequence.php
@@ -0,0 +1,24 @@
+<?php
+
+function fibonacci($number)
+{
+  $result = "";
+  $a = 0;
+  $b = 1;
+  $c;
+  if ($number >= 1){
+    $result .= "$a";
+  }
+  if ($number >= 2) {
+    $result .= ", $b";
+  }
+  for($i = 2; $i < $number; $i++){
+    $c = $a + $b;
+    $a = $b;
+    $b = $c;
+    $result .= ", $b";
+  }
+  return $result;
+}
+
+var_dump(fibonacci(1));
