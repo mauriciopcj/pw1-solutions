86ed11849a94c7fd88be8908c1f4f6e083c81818
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2019-09-06 22:20:26 -0300

    habilitando demais testes em function-calc

--- php-core/function-calc/mauricio.pereira/spec/calc.spec.php
+++ php-core/function-calc/mauricio.pereira/spec/calc.spec.php
@@ -6,14 +6,14 @@ describe('Calculator', function () {
     expect(calc(1, 1, '+'))->toBe(2);
   });
 
-  xit('subtracting 1 - 1', function () {
+  it('subtracting 1 - 1', function () {
     expect(calc(1, 1, '-'))->toBe(0);
   });
 
-  xit('multiplying 1 * 1', function () {
+  it('multiplying 1 * 1', function () {
     expect(calc(1, 1, '*'))->toBe(1);
   });
-  xit('dividing 1 / 1', function () {
+  it('dividing 1 / 1', function () {
     expect(calc(1, 1, '/'))->toBe(1);
   });
 });
