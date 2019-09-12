ba700a00bbac00721b8fa669cfe88428094ec6e8
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2019-09-12 11:23:36 -0300

    solution array-fibonacci-sequence

--- php-core/array-fibonacci-sequence/mauricio.pereira/spec/fibonacci-sequence.spec.php
+++ php-core/array-fibonacci-sequence/mauricio.pereira/spec/fibonacci-sequence.spec.php
@@ -6,19 +6,19 @@ describe('Fibonacci series', function () {
     expect(fibonacci(0))->toEqual([]);
   });
 
-  xit('making the first 1 numbers', function () {
+  it('making the first 1 numbers', function () {
     expect(fibonacci(1))->toEqual([0]);
   });
 
-  xit('making the first 2 numbers', function () {
+  it('making the first 2 numbers', function () {
     expect(fibonacci(2))->toEqual([0, 1]);
   });
 
-  xit('making the first 4 numbers', function () {
+  it('making the first 4 numbers', function () {
     expect(fibonacci(4))->toEqual([0, 1, 1, 2]);
   });
 
-  xit('making the first 6 numbers', function () {
+  it('making the first 6 numbers', function () {
     expect(fibonacci(6))->toEqual([0, 1, 1, 2, 3, 5]);
   });
 });
--- php-core/array-fibonacci-sequence/mauricio.pereira/src/fibonacci-sequence.php
+++ php-core/array-fibonacci-sequence/mauricio.pereira/src/fibonacci-sequence.php
@@ -2,5 +2,21 @@
 
 function fibonacci($number)
 {
-  // TODO
+  $result = [];
+  $a = 0;
+  $b = 1;
+  $c;
+  if ($number >= 1){
+    array_push($result, $a);
+  }
+  if ($number >= 2) {
+    array_push($result, $b);
+  }
+  for($i = 2; $i < $number; $i++){
+    $c = $a + $b;
+    $a = $b;
+    $b = $c;
+    array_push($result, $b);
+  }
+  return $result;
 }
