4e004433eee8f4e248fe83bc95a42451c0edc248
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2019-09-12 10:49:50 -0300

    solutino array-products-qtd

--- php-core/array-products-qtd/mauricio.pereira/src/products-qtd.php
+++ php-core/array-products-qtd/mauricio.pereira/src/products-qtd.php
@@ -2,15 +2,30 @@
 
 function gtZero($products)
 {
-  // TODO
+  for($i = sizeof($products) -1; $i >= 0; $i--){
+    if($products[$i][2] == 0){
+      unset($products[$i]);
+    }
+  }
+  return $products;
 }
 
 function subTotal($products)
 {
-  // TODO
+  $result = [];
+  foreach($products as $item){
+    $aux = [$item[0], $item[1] * $item[2]];
+    array_push($result, $aux);
+  }
+  return $result;
 }
 
 function total($products)
 {
-  // TODO
+  $result = 0;
+  $sub = subTotal($products);
+  foreach($sub as $item){
+    $result += $item[1];
+  }
+  return $result;
 }
