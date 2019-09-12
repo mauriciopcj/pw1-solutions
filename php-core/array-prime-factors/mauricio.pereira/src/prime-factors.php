8dda817e4cc6c5940d0218a5de7555207afb731d
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2019-09-12 11:46:44 -0300

    solution array-prime-factors

--- php-core/array-prime-factors/mauricio.pereira/src/prime-factors.php
+++ php-core/array-prime-factors/mauricio.pereira/src/prime-factors.php
@@ -1,6 +1,23 @@
 <?php
 
+require __DIR__ . '/../../../function-prime/mauricio.pereira/src/prime.php';
+
 function primeFactors($number)
 {
-  // TODO
+  $result = [];
+  $primos = [];
+  if($number !== 1){
+    foreach(range(2,$number) as $n){
+      if (isPrime($n)){
+        array_push($primos, $n);
+      }
+    }
+    foreach($primos as $valor){
+      while($number % $valor == 0){
+        array_push($result, $valor);
+        $number /= $valor;
+      }
+    }
+  }
+  return $result;
 }
