cb36811602a0319e6169c2b8a042db153edb3d2b
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2019-09-19 07:43:05 -0300

    Corrigindo ordenação do array de objetos

--- php-core/object-comics/mauricio.pereira/src/comic.php
+++ php-core/object-comics/mauricio.pereira/src/comic.php
@@ -5,14 +5,17 @@ class Comic
 {
   private static $characters = [];
 
-  public static function add($character){
+  public static function add(Character $character){
     array_push(self::$characters, $character);
+    usort(self::$characters, function($a, $b){
+      return strcasecmp($a->__get("hero"), $b->__get("hero"));
+    });
   }
 
-  public static function names($comic=""){
+  public static function names($comic=null){
     $result = [];
     foreach(self::$characters as $char){
-      if($comic != ""){
+      if($comic != null){
         if(strcmp($char->__get("comic"), $comic) == 0){
           array_push($result,$char->__get("hero"));
         }
@@ -23,11 +26,11 @@ class Comic
     return $result;
   }
 
-  public static function weights($comic=""){
+  public static function weights($comic=null){
     $weights = 0;
     $quant = 0;
     foreach(self::$characters as $char){
-      if($comic != ""){
+      if($comic != null){
         if(strcmp($char->__get("comic"), $comic) == 0){
           $weights += $char->__get("weight");
           $quant++;
